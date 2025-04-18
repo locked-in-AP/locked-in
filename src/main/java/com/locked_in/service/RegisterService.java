d1cf352cf9d187549ba9a7d80d24f7605d6c70c5
Prashanna Sthapit <prashannasthapit@outlook.com>
2025-04-18 20:31:47 +0545

    Removed unused import

--- src/main/java/com/locked_in/service/RegisterService.java
+++ src/main/java/com/locked_in/service/RegisterService.java
@@ -2,7 +2,6 @@ package com.locked_in.service;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 
